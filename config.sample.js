71eaecc4bf71f1189deb69e39a1e3b9d7282ffb3
Shane Tiberius Wolf & Thomas Herald Lotze <thomas@grockit.com>
2012-06-06 12:41:48 -0700

    fixing redis db num config

--- config.sample.js
+++ config.sample.js
@@ -10,10 +10,10 @@ config.db.password = 'password';
 config.db.database = 'metricizer';
 config.redis.host = '127.0.0.1';
 config.redis.port = 6379;
-config.redis_db_num = 0;
+config.redis.db_num = 0;
 config.msg_source_redis.host = '127.0.0.1';
 config.msg_source_redis.port = 6379;
-config.msg_source_redis.redis_db_num = 0;
+config.msg_source_redis.db_num = 0;
 config.mongo_db_name = process.env.NODE_ENV+'WhistlePunk';
 config.mongo_db_server = '127.0.0.1';
 config.mongo_db_port = 27017;
@@ -27,11 +27,11 @@ case 'production':
   break;
 case 'test':
   config.db.database = 'metricizer_test';
-  config.redis_db_num = 3;
+  config.redis.db_num = 3;
   break;
 case 'development':
   config.db.database = 'metricizer_dev';
-  config.redis_db_num = 2;
+  config.redis.db_num = 2;
   break;
 default:
   console.log("No configuration!!")
