82dbc920ff01e0ba14d18e9bd30ba5c1b043f320
Thomas Lotze <thomas@grockit.com>
2012-06-19 14:28:37 -0700

    add /usr/local/bin to PATH

--- script/execute_backup.sh
+++ script/execute_backup.sh
@@ -2,4 +2,6 @@
 . /etc/profile
 . /home/grockit/.profile
 
+export PATH=/usr/local/bin:$PATH
+
 NODE_ENV=production TZ=US/Pacific /opt/grockit/whistlepunk/shared/node_modules/.bin/coffee /opt/grockit/whistlepunk/current/script/stop_whistlepunk_and_dump_status.coffee
